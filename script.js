08eeb5505de17b7ce78941d2a60ce4ebdd51863e
DCbot <dcbot@foss.dk>
2021-12-06 23:52:11 +0100

    add recipe page

--- script.js
+++ script.js
@@ -11,29 +11,29 @@ var options = {
   },
 };
 
-axios
-  .request(options)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+// axios
+//   .request(options)
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
 
 //   Get recipe information
 var options1 = {
   method: "GET",
-  url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/1118357/information",
+  url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/482788/information",
   headers: {
     "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
     "x-rapidapi-key": "45577c3ba9msh94810407e5e0130p165a48jsnf26c9fe12809",
   },
 };
 
-axios
-  .request(options1)
-  .then(function (response) {
-    console.log(response.data);
+// axios
+//   .request(options1)
+//   .then(function (response) {
+//     console.log(response.data);
 
     // returns
     // data.analyzedInstructions[0].steps[map(step=>step.step)]
@@ -53,11 +53,11 @@ axios
     // data.spoonacularUrl
     // data.summary - text for the dish
     // data.title - name of the dish
-    // data.vegan, data.vegeratian, data.veryHealty, data.veryPopular, data.flutenFree - true or false
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+    // data.vegan, data.vegeratian, data.veryHealty, data.veryPopular, data.glutenFree - true or false
+  // })
+  // .catch(function (error) {
+  //   console.error(error);
+  // });
 
 // Extract the recipe
 var options4 = {
@@ -70,27 +70,22 @@ var options4 = {
   },
 };
 
-axios
-  .request(options4)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+// axios
+//   .request(options4)
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
 
-// Search for recipe
+// Search for recipe. Adding in Category
 var options3 = {
   method: "GET",
   url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search",
   params: {
-    query: "burger",
-    diet: "vegetarian",
-    excludeIngredients: "coconut",
-    intolerances: "egg, gluten",
-    number: "10",
-    offset: "0",
-    type: "main course",
+    query: "main dish",
+    number: "20",
   },
   headers: {
     "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
@@ -98,17 +93,17 @@ var options3 = {
   },
 };
 
-axios
-  .request(options3)
-  .then(function (response) {
-    console.log(response.data);
+// axios
+//   .request(options3)
+//   .then(function (response) {
+//     console.log(response.data);
 
     // returns
     // data.results[id,image,readyInMinutes,servings,sourceUrl,title]
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+  // })
+  // .catch(function (error) {
+  //   console.error(error);
+  // });
 
 // Get similar recipe
 
@@ -121,10 +116,10 @@ var options2 = {
   },
 };
 
-axios
-  .request(options2)
-  .then(function (response) {
-    console.log(response.data);
+// axios
+//   .request(options2)
+//   .then(function (response) {
+//     console.log(response.data);
 
     // returns
     // data.id
@@ -132,10 +127,10 @@ axios
     // data.servings
     // data.sourceUrl
     // data.title
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+  // })
+  // .catch(function (error) {
+  //   console.error(error);
+  // });
 
 // var options2 = {
 //   method: "GET",
@@ -154,3 +149,35 @@ axios
 //   .catch(function (error) {
 //     console.error(error);
 //   });
+
+
+
+// const mainFunction = (input) => {
+//   // Place your code here
+
+//   const array1 = [0,1,2,3,4,5,6,7,8,9]
+//   const array2 = Array.from(input)
+//   console.log(array2)
+//   // Loop for array1
+//   for(let i = 0; i < array1.length; i++) {
+       
+//       // Loop for array2
+//       for(let j = 0; j < array2.length; j++) {
+           
+//           // Compare the element of each and
+//           // every element from both of the
+//           // arrays
+//           if(array1[i] === array2[j]) {
+           
+//               // Return if common element found
+//               return true;
+//           }
+//       }
+//   }
+   
+//   // Return if no common element exist
+//   return false;
+
+// }
+
+// mainFunction('a0b1c2d3e4f5g6h7i8j9')
